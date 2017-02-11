e34406c61ba9abfc334bdc5479a1cdd4fa525275
rsghatpa <rsghatpa@ncsu.edu>
2017-02-10 19:52:21 -0500

    Deletion function added
    Deletion function added

--- my_text_editor.c
+++ my_text_editor.c
@@ -2,22 +2,19 @@
 
 #include"my_text_editor.h"
 
-text_node *get_node()
-{
+text_node *get_node(){
 	text_node *tmp;
 	nodes_taken += 1;
-	if (free_list != NULL)
-	{
+	if (free_list != NULL){
 		tmp = free_list;
 		free_list = free_list->right;
 	}
-	else
-	{
-		if (currentblock == NULL || size_left == 0)
-		{
+	else{
+		if (currentblock == NULL || size_left == 0){
 			currentblock = (text_node *)malloc(BLOCKSIZE * sizeof(text_node));
 			size_left = BLOCKSIZE;
 		}
+
 		tmp = currentblock++;
 		size_left -= 1;
 	}
@@ -25,8 +22,7 @@ text_node *get_node()
 }
 
 /* This function assigns the default values for newly created node */
-text_node *create_node(void)
-{
+text_node *create_node(void){
 	text_node *new_node;
 	new_node = get_node();
 	new_node->left = NULL;
@@ -47,13 +43,11 @@ text_node *create_text_file() {
 /* This function assigns the given line to a leaf node */
 char *create_object(char *line){
 	char *new_object = (char *)malloc(strlen(line) + 1);
-	if (new_object == NULL)
-	{
+	if (new_object == NULL){
 		perror("Malloc failed in create object");
 		return NULL;
 	}
 	strcpy(new_object, line);
-	printf("Line in create object %s\n",new_object);
 	return (new_object);
 }
 
@@ -82,7 +76,8 @@ void right_rotation(text_node *node){
 
 
 void preorder(text_node *text) {
-	if (text == NULL) {
+	if (text->right == NULL) {
+		printf("%s\t\t%s\t height : %d \t key : %d\n", (char *) text, (char *)text->left, text->height, text->key);
 		printf("Coming up\n");
 		return;
 	}
@@ -107,9 +102,6 @@ void append_line(text_node *text, char *new_line){
 	int traversal_complete  = 0;
 	if (text->left == NULL){
 		text->left = (text_node *)create_object(new_line);//ask barve
-		
-		text->left->left = NULL;
-		text->left->right = NULL;
 		text->right = NULL;
 		text->parent = NULL;
 		text->key = 1;
@@ -132,10 +124,6 @@ void append_line(text_node *text, char *new_line){
 		prev_leaf->height = 0;
 
 		next_leaf->left = (text_node *)create_object(new_line);
-//		printf("In append line %s\n", next_leaf->left);
-//		printf("In append line char %s\n", (char *)next_leaf->left);
-		next_leaf->left->left = NULL;
-		next_leaf->left->right = NULL;
 		next_leaf->key = tmp_node->key;
 		next_leaf->right = NULL;
 		next_leaf->parent = tmp_node;
@@ -172,8 +160,6 @@ void append_line(text_node *text, char *new_line){
 				}
 			}
 			else if (tmp_node->right->height - tmp_node->left->height == 2){//right subtree is longer than left subtree. Needs balancing 
-				//printf("Going on a preorder traversal before rotation\n\n**********************************\n");
-				//preorder(text);
 
 				if (tmp_node->right->right->height - tmp_node->left->height == 1){//LL rotation
 					left_rotation(tmp_node);
@@ -215,13 +201,8 @@ void append_line(text_node *text, char *new_line){
 }
 /* This function performs the inorder traversal of a given tree */
 void inorder(text_node *text){
-	if (text->right == NULL)	{
-/*			printf("************************\n");
-			printf("The key is %d\n", txt->key);
-			printf("The height is %d\n", txt->height);*/
+	if (text->right == NULL){
 			printf("%s\n", (char *)text->left);
-			//printf("The parent address is %d\n", txt->parent);
-			
 
 		return;
 	}
@@ -234,6 +215,7 @@ void inorder(text_node *text){
 	}
 }
 
+/* This function gets the line of number index, if such a line exists, and returns NULL else */
 text_node *get_to_line(text_node *text, int index) {
 	text_node *tmp_node = text;
 	if (text->key < index || index == 0 || text->key == 0) {
@@ -257,6 +239,7 @@ text_node *get_to_line(text_node *text, int index) {
 /* This function gets the line of number index, if such a line exists, and returns NULL else */
 char *get_line(text_node *text, int index){
 	text_node* tmp_node = get_to_line(text, index);
+	
 	return tmp_node == NULL ? NULL : (char *)(tmp_node->left);
 }
 
@@ -282,8 +265,6 @@ void insert_line(text_node *text, int index, char * new_line){
 	if (text->left == NULL)
 	{
 		text->left = (text_node *)create_object(new_line);
-		text->left->left = NULL;
-		text->left->right = NULL;
 		text->key = 1;
 		text->right = NULL;
 		text->parent = NULL;
@@ -319,8 +300,6 @@ void insert_line(text_node *text, int index, char * new_line){
 		prev_leaf->height = 0;
 
 		new_leaf->left = (text_node *)create_object(new_line);
-		new_leaf->left->left = NULL;
-		new_leaf->left->right = NULL;
 		new_leaf->key = 1;
 		new_leaf->right = NULL;
 		new_leaf->parent = tmp_node;
@@ -402,24 +381,144 @@ void insert_line(text_node *text, int index, char * new_line){
 
 }
 
+/* This function returns the deleted node to the free list */
+void return_node(text_node *node)
+{
+	node->right = free_list;
+	free_list = node;
+	nodes_returned += 1;
+}
+/* This function deletes the line of number index, renumbering all lines after that line, and returns a pointer to the deleted line */
+char * delete_line(text_node *text, int index){
+	text_node *tmp_node;
+	text_node *upper_node = NULL;
+	text_node *other_node = NULL;
+	char *deleted_object;
+	int count = index;
+	int finished;
+	tmp_node = text;
+	if (text->left == NULL || length_text(text) < index || index <= 0){
+		return NULL;
+	}
+	else if (text->right == NULL){
+		deleted_object = (char *)text->left;
+		text->left = NULL;
+		text->parent = NULL;
+		text->key = 0;
+		text->height = 0;
+		return (deleted_object);
+	}
+	else{
+		while (tmp_node->right != NULL){
+			upper_node = tmp_node;
+			(tmp_node->key)--;
+			if (count <= tmp_node->left->key){
+				tmp_node = tmp_node->left;
+				tmp_node = upper_node->left;
+				other_node = upper_node->right;
+			}
+			else{
+				count -= tmp_node->left->key;
+				tmp_node = tmp_node->right;
+				tmp_node = upper_node->right;
+				other_node = upper_node->left;
+			}
+		}
+		upper_node->key = other_node->key;
+		upper_node->left = other_node->left;
+		upper_node->right = other_node->right;
+		upper_node->height = other_node->height;
+
+		if (upper_node->right != NULL){
+			upper_node->left->parent = upper_node;
+			upper_node->right->parent = upper_node;
+		}
+		deleted_object = (text_node *)tmp_node->left;
+		return_node(tmp_node);
+		return_node(other_node);
+		upper_node = upper_node->parent;
+		tmp_node = upper_node;
+
+		finished = 0;
+		while (!finished && tmp_node != NULL){
+			int tmp_height, old_height;
+			old_height = tmp_node->height;
+			if (tmp_node->left->height - tmp_node->right->height == 2){
+				if (tmp_node->left->left->height - tmp_node->right->height == 1){
+					right_rotation(tmp_node);
+					tmp_node->right->height = tmp_node->right->left->height + 1;
+					tmp_node->height = tmp_node->right->height + 1;
+					tmp_node->right->key = tmp_node->right->left->key + tmp_node->right->right->key;
+					tmp_node->key = tmp_node->left->key + tmp_node->right->key;
+				}
+				else{
+					left_rotation(tmp_node->left);
+					right_rotation(tmp_node);
+					tmp_height = tmp_node->left->left->height;
+					tmp_node->left->height = tmp_height + 1;
+					tmp_node->right->height = tmp_height + 1;
+					tmp_node->height = tmp_height + 2;
+					tmp_node->left->key = tmp_node->left->left->key + tmp_node->left->right->key;
+					tmp_node->right->key = tmp_node->right->left->key + tmp_node->right->right->key;
+					tmp_node->key = tmp_node->left->key + tmp_node->right->key;
+				}
+			}
+			else if (tmp_node->left->height - tmp_node->right->height == -2){
+				if (tmp_node->right->right->height - tmp_node->left->height == 1){
+					left_rotation(tmp_node);
+					tmp_node->left->height = tmp_node->left->right->height + 1;
+					tmp_node->height = tmp_node->left->height + 1;
+					tmp_node->left->key = tmp_node->left->left->key + tmp_node->left->right->key;
+					tmp_node->key = tmp_node->left->key + tmp_node->right->key;
+				}
+				else{
+					right_rotation(tmp_node->right);
+					left_rotation(tmp_node);
+					tmp_height = tmp_node->right->right->height;
+					tmp_node->left->height = tmp_height + 1;
+					tmp_node->right->height = tmp_height + 1;
+					tmp_node->height = tmp_height + 2;
+					tmp_node->right->key = tmp_node->right->left->key + tmp_node->right->right->key;
+					tmp_node->left->key = tmp_node->left->left->key + tmp_node->left->right->key;
+					tmp_node->key = tmp_node->left->key + tmp_node->right->key;
+				}
+			}
+			else{
+				if (tmp_node->left->height > tmp_node->right->height){
+					tmp_node->height = tmp_node->left->height + 1;
+				}
+				else{
+					tmp_node->height = tmp_node->right->height + 1;
+				}
+			}
+			if (tmp_node->height == old_height){
+				finished = 1;
+			}
+			tmp_node = tmp_node->parent;
+		}
+
+		return (deleted_object);
+	}
+}
+
 int main() {
 	text_node *text1;
 	printf("Create a new text file\n");
 	text1 = create_text_file();
-	//printf("\n*******\nLength of text %d\n", length_text(text1));
+	
 	append_line(text1, "Hello");
-	//printf("\n*******\nLength of text %d\n", length_text(text1));
 	append_line(text1, "1234567890123456789");
 	append_line(text1, "Today");
 	append_line(text1, "It is windy");
-	//printf("\n*******\nLength of text %d\n", length_text(text1));
 	insert_line(text1, 2, "Line being inserted");
 	insert_line(text1, 12, "12th line");
-
-	//printf("Going for preorder \n");
-
-	//preorder(text1);
-
+	
+	printf("Going for preorder \n");
+	preorder(text1);
+	
+	//printf("Going for an inorder traversal\n");
+	//inorder(text1);
+	/*
 	printf("Total number of lines");
 	printf("\n*******\nLength of text %d\n", length_text(text1));
 	
@@ -428,6 +527,11 @@ int main() {
 	printf("Pointer to old line %s\n",set_line(text1, 5, "Test Set Line"));
 	printf("Fifth line is %s\n", get_line(text1, 5));
 	printf("Sixth line is %s\n", get_line(text1, 6));
+	
+	printf("Deleted line is %s\n", delete_line(text1, 0));
+	printf("First line is %s\n", get_line(text1, 0));
+	printf("First line is %s\n", get_line(text1, 1));
+	*/
 	getchar();
 	return 0;
 }
